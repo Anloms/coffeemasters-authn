662a275ba2d84e72eacf3c85bcdc802784dcefb6
Oweley <aleksandra.smolna@gmail.com>
2024-07-05 11:16:06 +0100

    defined backend routes and indentified a todo for the future

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Low } from 'lowdb'
+import { Low } from 'lowdb'//open source library for node that brights the content of json files; which is easy for debugging, we can see whether the content is stored or not
 import { JSONFile } from 'lowdb/node'
 import * as url from 'url';
 import bcrypt from 'bcryptjs';
@@ -28,8 +28,29 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-// ADD HERE THE REST OF THE ENDPOINTS
+function findUser(email) {
+  const results = db.data.users.filter(x=>x.email == email);
+  if(results.length===0) return undefined;
+  return results[0];
+}
 
+// ADD HERE THE REST OF THE ENDPOINTS
+app.post("/auth/register", (req,res) => {
+  //TODO: to make the code production ready: Data Validation
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }
+  const userFound = findUser(user.email);
+  if(userFound ) {
+    res.status(404).send({ok:false, message: "user already exists"})
+  } else{
+    db.data.users.push(user);
+    db.write();
+    res.status(200).json(user)
+  }
+})
 
 
 app.get("*", (req, res) => {
